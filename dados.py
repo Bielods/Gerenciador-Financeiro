ebec171588b12ce766a869c860eeb1df24051be1
Gabriel Oliveira <gabrieloliveira21208@gmail.com>
2026-05-20 23:51:11 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -2,5 +2,5 @@ transacoes= []
 fila_pendentes= []
 pilha_pagamentos= []
 TIPOS= ('receita', 'despesa')
-STATUS= ('pendente', 'pago')
+STATUS= ('Pendente', 'Pago')
 CATEGORIAS= ('alimentação', 'transporte', 'saúde', 'educação', 'lazer', 'outros', 'ganhos')
